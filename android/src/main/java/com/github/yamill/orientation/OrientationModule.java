0121450816b2bcb6f831dcc3b2e6548aae568f9a
WebMobileDeveloper <vladdragonsun@gmail.com>
2018-07-07 14:52:20 -0700

    add sensorOrientationChangeEvent

---
+++ android/src/main/java/com/github/yamill/orientation/OrientationModule.java
@@ -0,0 +1,269 @@
+package com.github.yamill.orientation;
+
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.util.Log;
+
+import com.facebook.common.logging.FLog;
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.LifecycleEventListener;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.common.ReactConstants;
+import com.facebook.react.modules.core.DeviceEventManagerModule;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.annotation.Nullable;
+
+public class OrientationModule extends ReactContextBaseJavaModule implements LifecycleEventListener, SensorEventListener{
+
+    private SensorManager sensorManager;
+    private Sensor sensor;
+    public static final int PORTRAIT = 1;
+    public static final int LANDSCAPE_RIGHT = 2;
+    public static final int UPSIDE_DOWN = 3;
+    public static final int LANDSCAPE_LEFT = 4;
+    public int mOrientationDeg; //last rotation in degrees
+    public int mOrientationRounded; //last orientation int from above
+    private static final int _DATA_X = 0;
+    private static final int _DATA_Y = 1;
+    private static final int _DATA_Z = 2;
+    private int ORIENTATION_UNKNOWN = -1;
+    private int tempOrientRounded = 0;
+          
+    final BroadcastReceiver receiver;
+    private ReactApplicationContext context;
+    public OrientationModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+        final ReactApplicationContext ctx = reactContext;
+        context = ctx;
+        receiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                Configuration newConfig = intent.getParcelableExtra("newConfig");
+                Log.d("receiver", String.valueOf(newConfig.orientation));
+
+                String orientationValue = newConfig.orientation == 1 ? "PORTRAIT" : "LANDSCAPE";
+
+                WritableMap params = Arguments.createMap();
+                params.putString("orientation", orientationValue);
+                if (ctx.hasActiveCatalystInstance()) {
+                    ctx
+                    .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+                    .emit("orientationDidChange", params);
+                }
+            }
+        };
+        ctx.addLifecycleEventListener(this);
+        sensorManager = (SensorManager) ctx.getSystemService(ctx.SENSOR_SERVICE);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+    }
+
+    @Override
+    public String getName() {
+        return "Orientation";
+    }
+
+    @ReactMethod
+    public void getOrientation(Callback callback) {
+        final int orientationInt = getReactApplicationContext().getResources().getConfiguration().orientation;
+
+        String orientation = this.getOrientationString(orientationInt);
+
+        if (orientation == "null") {
+            callback.invoke(orientationInt, null);
+        } else {
+            callback.invoke(null, orientation);
+        }
+    }
+
+    @ReactMethod
+    public void lockToPortrait() {
+        final Activity activity = getCurrentActivity();
+        if (activity == null) {
+            return;
+        }
+        activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+    }
+
+    @ReactMethod
+    public void lockToLandscape() {
+        final Activity activity = getCurrentActivity();
+        if (activity == null) {
+            return;
+        }
+        activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR_LANDSCAPE);
+    }
+
+    @ReactMethod
+    public void lockToLandscapeLeft() {
+        final Activity activity = getCurrentActivity();
+        if (activity == null) {
+            return;
+        }
+        activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+    }
+
+    @ReactMethod
+    public void lockToLandscapeRight() {
+        final Activity activity = getCurrentActivity();
+        if (activity == null) {
+            return;
+        }
+        activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_REVERSE_LANDSCAPE);
+    }
+
+    @ReactMethod
+    public void unlockAllOrientations() {
+        final Activity activity = getCurrentActivity();
+        if (activity == null) {
+            return;
+        }
+        activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR);
+    }
+
+    @Override
+    public @Nullable Map<String, Object> getConstants() {
+        HashMap<String, Object> constants = new HashMap<String, Object>();
+        int orientationInt = getReactApplicationContext().getResources().getConfiguration().orientation;
+
+        String orientation = this.getOrientationString(orientationInt);
+        if (orientation == "null") {
+            constants.put("initialOrientation", null);
+        } else {
+            constants.put("initialOrientation", orientation);
+        }
+
+        return constants;
+    }
+
+    private String getOrientationString(int orientation) {
+        if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            return "LANDSCAPE";
+        } else if (orientation == Configuration.ORIENTATION_PORTRAIT) {
+            return "PORTRAIT";
+        } else if (orientation == Configuration.ORIENTATION_UNDEFINED) {
+            return "UNKNOWN";
+        } else {
+            return "null";
+        }
+    }
+
+    @Override
+    public void onHostResume() {
+        final Activity activity = getCurrentActivity();
+
+        if (activity == null) {
+            FLog.e(ReactConstants.TAG, "no activity to register receiver");
+            return;
+        }
+        activity.registerReceiver(receiver, new IntentFilter("onConfigurationChanged"));
+        sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+    @Override
+    public void onHostPause() {
+        final Activity activity = getCurrentActivity();
+        if (activity == null) return;
+        try
+        {
+            activity.unregisterReceiver(receiver);
+            sensorManager.unregisterListener(this);
+        }
+        catch (java.lang.IllegalArgumentException e) {
+            FLog.e(ReactConstants.TAG, "receiver already unregistered", e);
+        }
+    }
+
+    @Override
+    public void onHostDestroy() {
+    }
+    @Override
+public void onSensorChanged(SensorEvent sensorEvent) {
+
+            float[] values = sensorEvent.values;
+            int orientation = ORIENTATION_UNKNOWN;
+            float X = -values[_DATA_X];
+            float Y = -values[_DATA_Y];
+            float Z = -values[_DATA_Z];
+            float magnitude = X*X + Y*Y;
+
+            if (magnitude * 4 >= Z*Z) {
+                float OneEightyOverPi = 57.29577957855f;
+                float angle = (float)Math.atan2(-Y, X) * OneEightyOverPi;
+                orientation = 90 - (int)Math.round(angle);
+                // normalize to 0 - 359 range
+                while (orientation >= 360) {
+                    orientation -= 360;
+                }
+                while (orientation < 0) {
+                    orientation += 360;
+                }
+            }
+
+            if (orientation != mOrientationDeg)
+            {
+                mOrientationDeg = orientation;
+                if(orientation == -1){//basically flat
+                }
+                else if(orientation < 10 || orientation > 350){
+                    tempOrientRounded = 1;//portrait
+                }
+                else if(orientation > 80 && orientation < 100){
+                    tempOrientRounded = 2; //lsRight
+                }
+                else if(orientation > 170 && orientation < 190){
+                    tempOrientRounded = 3; //upside down
+                }
+                else if(orientation > 260 && orientation < 280){
+                    tempOrientRounded = 4;//lsLeft
+                }
+            }
+            if(mOrientationRounded != tempOrientRounded){
+                mOrientationRounded = tempOrientRounded;
+                WritableMap map = Arguments.createMap();
+                switch (mOrientationRounded){
+                    case PORTRAIT:
+                        map.putString("orientation", "PORTRAIT");
+                        sendEvent(context, "sensorOrientationChangeEvent", map);
+                        break;
+                    case LANDSCAPE_LEFT:
+                        map.putString("orientation", "LANDSCAPE-LEFT");
+                        sendEvent(context, "sensorOrientationChangeEvent", map);
+                        break;
+                    case UPSIDE_DOWN:
+                        map.putString("orientation", "PORTRAITUPSIDEDOWN");
+                        sendEvent(context, "sensorOrientationChangeEvent", map);
+                        break;
+                    case LANDSCAPE_RIGHT:
+                        map.putString("orientation", "LANDSCAPE-RIGHT");
+                        sendEvent(context, "sensorOrientationChangeEvent", map);
+                        break;
+                }
+            }
+}
+@Override
+public void onAccuracyChanged(Sensor sensor, int i) {
+}
+private void sendEvent(ReactContext reactContext,
+                       String eventName,
+                       @Nullable WritableMap params) {
+    reactContext
+            .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+            .emit(eventName, params);
+}
+}
